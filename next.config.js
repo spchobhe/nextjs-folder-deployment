02d745f0ffb20ecb66afda27f3aaf35c8508e12b
spchobhe <spchobhe@live.com>
2023-06-26 08:47:38 -0400

    worked on the deployment testing

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+      reactStrictMode: true,
+      distDir: 'docs',
+       // Add basePath
+      output: 'export',
+}
+
+module.exports = nextConfig
