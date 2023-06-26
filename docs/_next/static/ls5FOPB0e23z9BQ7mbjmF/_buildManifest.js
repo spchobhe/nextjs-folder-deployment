11437d7605b17f68f205758f523f28eaeff05bd3
spchobhe <spchobhe@live.com>
2023-06-26 12:45:35 -0400

    Worked on the deployment

--- docs/_next/static/ls5FOPB0e23z9BQ7mbjmF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/aa41fb1ba1818c75.css","static/chunks/pages/index-1cf5a02be3b4e431.js"],"/_error":["static/chunks/pages/_error-3986dd5834f581dc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
