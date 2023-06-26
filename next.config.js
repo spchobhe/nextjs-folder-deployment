6dbd8072f6130e124e7c23fd049a36ea3b23ce13
spchobhe <spchobhe@live.com>
2023-06-26 08:53:49 -0400

    worked on the deployment testing

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
       distDir: 'docs',
        // Add basePath
       output: 'export',
+      // Add basePath
+      basePath: '/nextjs-folder-deployment',      
 }
 
 module.exports = nextConfig
